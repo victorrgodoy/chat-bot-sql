df7e1544612fb5e827fc00434d8f204fcd7311fc
Victor <victor.hgodoy@icloud.com>
2024-07-06 19:21:35 -0300

    atualizacao ChatGUI

--- src/main/java/gui/ChatGUI.java
+++ src/main/java/gui/ChatGUI.java
@@ -1,5 +1,4 @@
 package gui;
-
 import service.OllamaService;
 import util.OutputStyles;
 
@@ -9,6 +8,7 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+
 public class ChatGUI extends JFrame {
     private final MainGUI mainGUI;
     private JButton sentButton;
@@ -17,10 +17,10 @@ public class ChatGUI extends JFrame {
     private final String infoDatabase;
     private final String llmSelected;
     private JTextField input;
-    private String conversation = " ";
+    private  String conversation = " ";
     private JEditorPane output;
 
-    public ChatGUI(String databaseSelected, String llmSelected, String infoDatabase) {
+    public ChatGUI(String databaseSelected,String llmSelected ,String infoDatabase){
         this.mainGUI = MainGUI.getInstance();
         this.infoDatabase = infoDatabase;
         this.llmSelected = llmSelected;
@@ -29,14 +29,14 @@ public class ChatGUI extends JFrame {
         implementActionButtons();
     }
 
-    private void createComponents() {
+    private void createComponents(){
         setTitle("Chat bot");
         setSize(600, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
         container();
-        conversation += "<b>Modelo:   </b>" + llmSelected + "<br><br>";
+        conversation += "<b>Modelo selecionado:   </b>" + llmSelected + "<br><br>";
         conversation += "<b>Bot:</b> Olá, eu sou o SQL bot, faça uma pergunta:<br><br>";
         add(input = jTextField());
         output = jEditorPane();
@@ -58,7 +58,7 @@ public class ChatGUI extends JFrame {
         setVisible(true);
     }
 
-    private void implementActionButtons() {
+    private void implementActionButtons(){
         backButton.addActionListener(_ -> {
             mainGUI.setVisible(true);
             setVisible(false);
@@ -73,7 +73,7 @@ public class ChatGUI extends JFrame {
         output.setText(conversation);
 
         new Thread(() -> {
-            String sql = OllamaService.sendOllamaQuestion(infoDatabase, question, llmSelected);
+            String sql = OllamaService.sendOllamaQuestion(infoDatabase, question,llmSelected);
             String result = OllamaService.resultSQL(databaseSelected, sql);
             conversation += "<b>Bot:</b> " + result + "<br><br>";
 
@@ -81,14 +81,15 @@ public class ChatGUI extends JFrame {
         }).start();
     }
 
+
     //----Components-----
-    private void container() {
+    private void container(){
         Container container = getContentPane();
         container.setBackground(Color.white);
         container.setLayout(null);
     }
 
-    private JTextField jTextField() {
+    private JTextField jTextField(){
         JTextField jTextField = new JTextField();
         jTextField.setBounds(40, 400, 410, 40);
         jTextField.setFont(new Font("Arial", Font.PLAIN, 16));
@@ -96,7 +97,7 @@ public class ChatGUI extends JFrame {
         return jTextField;
     }
 
-    private JEditorPane jEditorPane() {
+    private JEditorPane jEditorPane(){
         HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
         OutputStyles outputStyles = new OutputStyles();
         outputStyles.ApplyStyles(htmlEditorKit);
@@ -109,39 +110,39 @@ public class ChatGUI extends JFrame {
         return jEditorPane;
     }
 
-    private JScrollPane jScrollPane(JEditorPane jEditorPane) {
+    private JScrollPane jScrollPane(JEditorPane jEditorPane){
         JScrollPane jScrollPane = new JScrollPane(jEditorPane);
-        jScrollPane.setBounds(40, 55, 525, 330);
+        jScrollPane.setBounds(40,55,525,330);
         jScrollPane.setForeground(Color.blue);
         jScrollPane.setBorder(null);
         return jScrollPane;
     }
 
-    private JLabel jLabel(String text, int y) {
+    private JLabel jLabel(String text, int y){
         JLabel jLabel = new JLabel(text);
-        jLabel.setBounds(44, y, 500, 35);
-        jLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        jLabel.setBounds(44,y,500,35);
+        jLabel.setFont(new Font("Arial", Font.BOLD,14));
         jLabel.setForeground(Color.black);
         return jLabel;
     }
 
-    private JLabel image() {
-        ImageIcon imageIcon = new ImageIcon("src/main/resources/static/img/logo.jpg");
-        Image image = imageIcon.getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
+    private JLabel image(){
+        ImageIcon imageIcon = new ImageIcon("src/main/resources/static/img/online.png");
+        Image image = imageIcon.getImage().getScaledInstance(25,25,Image.SCALE_SMOOTH);
         imageIcon = new ImageIcon(image);
         JLabel jLabel = new JLabel();
         jLabel.setIcon(imageIcon);
-        jLabel.setBounds(252, 30, 90, 90);
+        jLabel.setBounds(160,21,25,25);
         return jLabel;
     }
 
-    private JButton jButton(String text, int y) {
+    private JButton jButton(String text, int y){
         JButton jButton = new JButton(text);
-        jButton.setBounds(190, y, 220, 40);
-        jButton.setForeground(new Color(0, 0, 0, 153));
-        jButton.setFont(new Font("Arial", Font.BOLD, 16));
+        jButton.setBounds(470,y, 90, 36);
+        jButton.setFont(new Font("Arial", Font.BOLD, 12));
         jButton.setOpaque(true);
         jButton.setBackground(Color.white);
         return jButton;
     }
+
 }
